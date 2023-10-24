feac7634d99db9ccd2fa7630fe0373913ddc331c
angelmi11 <angelgnr11@gmail.com>
2023-10-24 09:30:39 -0500

    aa/IN-01 #PR #ESTRUCTURA DEL PROYECTO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CardComponent } from './shared/components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
